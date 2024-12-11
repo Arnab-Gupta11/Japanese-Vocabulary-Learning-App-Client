40381a005e7e3df45ee41bdefdc0c7360a9774c1
Arnab Gupta <arnabgupta1111@gmail.com>
2024-12-11 17:02:51 +0600

    Create dashboard layout

--- src/layout/AdminLayout.jsx
+++ src/layout/AdminLayout.jsx
@@ -1,21 +1,50 @@
-import { useEffect } from "react";
-import { useSelector } from "react-redux";
-import { Outlet, useNavigate } from "react-router";
+// import { Outlet } from "rea";
+
+import { useMediaQuery } from "@uidotdev/usehooks";
+// import { useClickOutside } from "@/hooks/use-click-outside";
+
+// import { Sidebar } from "@/layouts/sidebar";
+// import { Header } from "@/layouts/header";
+
+// import { cn } from "@/utils/cn";
+import { useEffect, useRef, useState } from "react";
+import { useClickOutside } from "../hooks/useClickOutside";
+import cn from "../lib/cn";
+import { Outlet } from "react-router";
+import { Header } from "../components/admin/Header/Header";
+import { Sidebar } from "../components/admin/Sidebar/Sidebar";
 
 const AdminLayout = () => {
-  const user = useSelector((state) => state.Auth.user);
-  const navigate = useNavigate();
-  // console.log(user)
+  const isDesktopDevice = useMediaQuery("(min-width: 768px)");
+  const [collapsed, setCollapsed] = useState(!isDesktopDevice);
+
+  const sidebarRef = useRef(null);
 
   useEffect(() => {
-    if (!user || user.role !== "admin") {
-      navigate("/login");
+    setCollapsed(!isDesktopDevice);
+  }, [isDesktopDevice]);
+
+  useClickOutside([sidebarRef], () => {
+    if (!isDesktopDevice && !collapsed) {
+      setCollapsed(true);
     }
-  }, [user]);
+  });
+
   return (
-    <div>
-      AdminLayout
-      <Outlet />
+    <div className="min-h-screen bg-slate-100 transition-colors dark:bg-slate-950">
+      <div
+        className={cn(
+          "pointer-events-none fixed inset-0 -z-10 bg-black opacity-0 transition-opacity",
+          !collapsed && "max-md:pointer-events-auto max-md:z-50 max-md:opacity-30"
+        )}
+      />
+      <Sidebar ref={sidebarRef} collapsed={collapsed} />
+      <div className={cn("transition-[margin] duration-300", collapsed ? "md:ml-[70px]" : "md:ml-[240px]")}>
+        <Header collapsed={collapsed} setCollapsed={setCollapsed} />
+        <div className="h-[calc(100vh-60px)] overflow-y-auto overflow-x-hidden p-6">
+          <Outlet />
+        </div>
+      </div>
     </div>
   );
 };
